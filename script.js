87e8b85e800e98fd3835e56a8f22c8dedb24631e
Kaleb Moore <k4l3bmoore@gmail.com>
2022-05-10 16:00:52 -0600

    Create function to get choice from player

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ let computerPlay = () => {
     let random = Math.floor(Math.random() * 3) + 1;
 
     let computer;
-    
+
     if (random === 1){
         computer = "Rock"
     } else if (random === 2){
         computer = "Paper"
     } else {
         computer = "Scissors"
+    }
+
+    return computer;
+}
+
+let playerPlay = () => {
+    let answer = prompt("Please choose Rock, Paper or Scissors: ").toLowerCase();
+
+    if(answer === "rock" || answer === "paper" || answer === "scissors") {
+        console.log(answer);
+    } else {
+        playerPlay();
     }
 }
