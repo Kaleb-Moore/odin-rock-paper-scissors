4b380b28aae0ee4a69e2e13546e37ad4a5bbf9bd
Kaleb Moore <k4l3bmoore@gmail.com>
2022-05-10 16:12:44 -0600

    Create playRound to play a game of rock paper scissors

--- script.js
+++ script.js
@@ -18,8 +18,38 @@ let playerPlay = () => {
     let answer = prompt("Please choose Rock, Paper or Scissors: ").toLowerCase();
 
     if(answer === "rock" || answer === "paper" || answer === "scissors") {
-        console.log(answer);
+        return answer;
     } else {
         playerPlay();
+    }
+}
+
+let playRound = (playerSelection = playerPlay(), computerSelection = computerPlay()) => {
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection === "Rock") {
+                console.log("It's a tie!")
+            } else if (computerSelection === "Paper") {
+                console.log("Computer Wins! Paper beats Rock,")
+            } else {
+                console.log("You Win! Rock beats Scissors.")
+            }
+            break;
+        case "paper":
+            if (computerSelection === "Rock") {
+                console.log("You Win! Paper beats Rock.")
+            } else if (computerSelection === "Paper") {
+                console.log("It's a tie!")
+            } else {
+                console.log("Computer Wins! Scissors beats Paper.")
+            }
+        case "scissors":
+            if (computerSelection === "Rock") {
+                console.log("Computer Wins! Rock beats Scissors.")
+            } else if (computerSelection === "Paper") {
+                console.log("You Win! Scissors beats Paper.")
+            } else {
+                console.log("It's a tie!")
+            }
     }
 }
