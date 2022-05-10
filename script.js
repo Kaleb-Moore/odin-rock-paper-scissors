080ddeeaef73d0a69d76de175e19576913d8499c
Kaleb Moore <k4l3bmoore@gmail.com>
2022-05-10 15:47:05 -0600

    Update use random to get rock paper or scissors

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 let computerPlay = () => {
     let random = Math.floor(Math.random() * 3) + 1;
+
+    let computer;
+    
+    if (random === 1){
+        computer = "Rock"
+    } else if (random === 2){
+        computer = "Paper"
+    } else {
+        computer = "Scissors"
+    }
 }
